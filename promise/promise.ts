ee6d2b94ae2bf2ee05ccfe185478934ccc681c01
Roy Segall <roy@segall.io>
2025-01-15 15:26:57 +0200

    Adding more
    Signed-off-by: Roy Segall <roy@segall.io>

---
+++ promise/promise.ts
@@ -0,0 +1,17 @@
+const delay = () => {
+    // Implement me.
+}
+
+(() => {
+    const start = new Date();
+    console.log(start);
+
+    const end = new Date();
+    console.log(end);
+
+    const diff = end.getSeconds() - start.getUTCSeconds();
+
+    if (diff <= 2.9) {
+        console.log('Not enough time passed 😭')
+    }
+})();
